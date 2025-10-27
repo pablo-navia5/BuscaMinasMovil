41203c296799f19a3ec9bbc10a05dfd2dd0681c9
pablo-navia5 <pablo.navia@educa.madrid.org>
2025-10-27 09:56:59 +0100

    funcion recursiva añadida

--- app/src/main/java/com/example/buscaminas/MainActivity.kt
+++ app/src/main/java/com/example/buscaminas/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -48,6 +50,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 
 fun BuscaMinas(modifier: Modifier = Modifier) {
@@ -99,23 +102,19 @@ fun BuscaMinas(modifier: Modifier = Modifier) {
                 modifier = modifier.padding(4.dp)
             ) {
                 items(100) { index ->
-                    val color = if (minas[index]) Color.Red else Color.DarkGray
-                    //val color = Color.Gray
-
                     Button(
                         onClick = {
-                            textos = textos.toMutableList().also {
-                                var cantidadMinas = cantidadMinasVecinas(index, minas)
-                                it[index] = cantidadMinas.toString()
-
-                                if (cantidadMinas == 0) {
-
+                            textos = textos.toMutableList().also { nuevosTextos ->
+                                if (minas[index]) {
+                                    nuevosTextos[index] = "💣"
+                                } else {
+                                    revelarCasillas(index, nuevosTextos, minas)
                                 }
                             }
                         },
                         shape = RectangleShape,
                         colors = ButtonDefaults.buttonColors(
-                            containerColor = color,
+                            containerColor = Color.DarkGray,
                             contentColor = Color.White
                         ),
                         contentPadding = PaddingValues(0.dp),
@@ -123,6 +122,38 @@ fun BuscaMinas(modifier: Modifier = Modifier) {
                             .aspectRatio(1f)
                     ) {
                         Text (textos[index])
+                    }
+                }
+            }
+        }
+    }
+}
+
+// Dado un index y el array de textos pone cuantas minas hay alrededor y llama de forma recursiva en caso de que no haya ninguna cerca.
+fun revelarCasillas (index: Int, textosActuales: MutableList<String>, minas: MutableList<Boolean>) {
+    // Si ya está revelada vuelve
+    if (textosActuales[index].isNotEmpty()) return
+
+    // Calcula cuántas minas hay alrededor
+    val cantidad = cantidadMinasVecinas(index, minas)
+    textosActuales[index] = cantidad.toString() //pone en los textos cuantas minas hay
+
+    // Si no hay minas vecinas revela sus vecinas
+    if (cantidad == 0) {
+        val fila = index / 10
+        val columna = index % 10
+
+        //doble for que recorre las casillas vecinas
+        for (filaRelativa in -1..1) {
+            for (columnaRelativa in -1..1) {
+                if (filaRelativa == 0 && columnaRelativa == 0) continue
+                val nuevaFila = fila + filaRelativa
+                val nuevaColumna = columna + columnaRelativa
+
+                if (nuevaFila in 0..9 && nuevaColumna in 0..9) {
+                    val nuevoIndex = nuevaFila * 10 + nuevaColumna
+                    if (!minas[nuevoIndex]) { //si no hay una mina llama recursivamente a la funcion
+                        revelarCasillas(nuevoIndex, textosActuales, minas)
                     }
                 }
             }
@@ -140,25 +171,25 @@ fun generarMinas(): MutableList<Boolean> {
     return lista
 }
 
+//Funcion que devuelve cuantas minas hay alrededor de un index.
 fun cantidadMinasVecinas(index: Int, minas: MutableList<Boolean>): Int {
     var cantidadMinas = 0
 
-    //esto calcula en que fila y columna
     val fila = index / 10
     val columna = index % 10
 
-    // Recorremos las 8 posiciones vecinas haciendo que la posicion sea relativa (es decir 0,0 es la actual y 0,1 sera la derecha)
-    for (filaRelativa in -1..1) { //recorre las 3 filas (-1 es la de arriba, 0 es la que esta el boton y  1 es la de abajo)
-        for (columnaRelativa in -1..1) { //recorre las 3 columnas (-1 es la de izquierda, 0 es la del boton y 1 es la de derecha)
-            if (filaRelativa == 0 && columnaRelativa == 0) continue // si estamos en la 0,0 es la actual asi que no la miramos.
+    // Recorremos las 8 posiciones vecinas
+    for (filaRelativa in -1..1) { //recorre las 3 filas
+        for (columnaRelativa in -1..1) { //por cada fila recorre las 3 columnas
+            if (filaRelativa == 0 && columnaRelativa == 0) continue // si estamos en la 0,0 significa que es la actual asi que pasamos a la siguiente
 
-            //comvertimos la posicion relativa al boton en la posicion absoluta en el tablero.
+            //comvertimos la posicion en la fila y columna del tablero.
             val nuevaFila = fila + filaRelativa
             val nuevaColumna = columna + columnaRelativa
 
-            // Verificar que esté dentro de los límites del tablero
+            // Verifica que este dentro de los limites del tablero
             if (nuevaFila in 0..9 && nuevaColumna in 0..9) {
-                val nuevoIndex = nuevaFila * 10 + nuevaColumna //el nuevo index del array la fila es *10 porque cada fila tiene 10 columnas.
+                val nuevoIndex = nuevaFila * 10 + nuevaColumna //creo el nuevo index del array la fila es *10 porque cada fila tiene 10 columnas.
                 if (minas[nuevoIndex]) cantidadMinas++ //si hay mina se suma
             }
         }
